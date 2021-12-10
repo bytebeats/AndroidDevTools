50def73f03cec343d8802fe4c72a07fd86498c7a
Chen Pan <happychinapc@gmail.com>
2021-12-10 15:12:11 +0800

    Initial commit

---
+++ screen-recorder/src/main/java/me/bytebeats/tools/recorder/ScreenRecorderService.kt
@@ -0,0 +1,18 @@
+package me.bytebeats.tools.recorder
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+/**
+ * Created by bytebeats on 2021/12/10 : 15:04
+ * E-mail: happychinapc@gmail.com
+ * Quote: Peasant. Educated. Worker
+ */
+class ScreenRecorderService : Service() {
+
+    override fun onBind(intent: Intent?): IBinder? = null
+
+
+
+}
